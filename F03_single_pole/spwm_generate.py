f9474f5512d68fa6cffb00ec65ba3354da34a640
GeekerRicardo <62746665+GeekerRicardo@users.noreply.github.com>
2020-03-29 14:43:13 +0800

    again

--- F03_single_pole/spwm_generate.py
+++ F03_single_pole/spwm_generate.py
@@ -2,6 +2,9 @@ import math
 
 
 #定义求占空比的函数
+#period=cubemx.tim.conter period
+#A,B分别为正弦波的幅值和，脉冲的幅值
+#f，f1是正弦波的频率和时间块的频率
 def get_spwm(period,A,B,f,f1):
     num=int(f1/(2*f)+1)
     for k in range(1,num):
